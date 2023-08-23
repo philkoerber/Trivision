46971e49b4aa483c5c29383f272140c7df063a4b
philkoerber <philipp.koerber.93@gmail.com>
2023-08-23 15:53:46 +0900

    switch from useSpring to animate of framer-motion-3d. that avoids problems with deployment

--- app/3DObjects/Card.jsx
+++ app/3DObjects/Card.jsx
@@ -1,19 +1,20 @@
-import { useSpring, animated, config } from '@react-spring/three'
+import { motion } from 'framer-motion-3d'
 import { useTexture } from '@react-three/drei'
 import { generateCardLink, tarotDeck } from './tarotDeckArray'
 
 const Card = ({ card, active }) => {
-  const springs = useSpring({ rotation: active ? 0 : Math.PI })
-  const { rotation } = useSpring({
-    rotation: active ? 0 : Math.PI,
-    config: config.molasses,
-  })
+  const rotation = active ? 0 : Math.PI
 
   const pic = generateCardLink(card)
   const frontTexture = useTexture(pic)
 
   return (
-    <animated.mesh rotation-y={rotation} position={[0, 0, 0]}>
+    <motion.mesh
+      initial={{ rotateY: rotation }}
+      animate={{ rotateY: rotation }}
+      transition={{ duration: 1, ease: 'easeInOut' }}
+      position={[0, 0, 0]}
+    >
       <boxGeometry args={[1.75, 3, 0.02]} />
       <meshBasicMaterial attach='material-0' color={'black'} />
       <meshBasicMaterial attach='material-1' color={'black'} />
@@ -23,7 +24,7 @@ const Card = ({ card, active }) => {
       <meshStandardMaterial attach='material-5' color={'#343233'} metalness={1} roughness={0.3} />
       {/* frontside */}
       <meshBasicMaterial attach='material-4' map={frontTexture} />
-    </animated.mesh>
+    </motion.mesh>
   )
 }
 
--- app/reading/Meaning.jsx
+++ app/reading/Meaning.jsx
@@ -8,20 +8,17 @@ function Meaning() {
   const reading = useStore((state) => state.reading)
   const readingState = useStore((state) => state.readingState)
 
-  useEffect(() => {
-    console.log('reading state: ' + readingState)
-    if (reading) console.log(reading.cards[readingState - 1])
-  }, [readingState])
+  useEffect(() => {}, [readingState])
 
   return (
     <AnimatePresence>
       <motion.div
         className='absolute w-screen h-screen flex items-center justify-center'
         key={readingState}
-        initial={{ opacity: 0, scale: 0.5, filter: 'blur(20px)', x: 100 }}
+        initial={{ opacity: 0, scale: 0.5, filter: 'blur(10px)', x: 200 }}
         animate={{ opacity: 1, scale: 1, filter: 'blur(0px)', x: 0 }}
-        exit={{ opacity: 0, scale: 0.5, filter: 'blur(20px)', x: -100 }}
-        transition={{ duration: 2, ease: 'easeInOut' }}
+        exit={{ opacity: 0, scale: 0.5, filter: 'blur(10px)', x: -200 }}
+        transition={{ duration: 2, ease: 'backInOut' }}
       >
         <div className='relative w-[300px] h-fit text-neutral-200 bg-neutral-800 bg-opacity-90 flex justify-center items-center p-8'>
           {readingState > 0 ? (readingState !== 4 ? reading.cards[readingState - 1].meaning : reading.meaning) : null}
