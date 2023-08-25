8b6d96853c9f4da6d1a57536cb867a845dcafc14
philkoerber <philipp.koerber.93@gmail.com>
2023-08-26 01:05:31 +0900

    update buttons

--- app/reading/Meaning.jsx
+++ app/reading/Meaning.jsx
@@ -40,12 +40,12 @@ function Meaning() {
         }}
       >
         <div className='relative rounded-lg w-screen max-w-[600px] text-lg h-fit border-4 border-neutral-900 text-neutral-800 bg-neutral-300 bg-opacity-90 flex justify-center items-center p-6'>
-          <button disabled={animating} onClick={() => handleButton('backward')}>
-            before
+          <button className='p-8 text-lg font-bold' disabled={animating} onClick={() => handleButton('backward')}>
+            {'<'}
           </button>
           {readingState > 0 ? (readingState !== 4 ? reading.cards[readingState - 1].meaning : reading.meaning) : null}
-          <button disabled={animating} onClick={() => handleButton('forward')}>
-            next
+          <button className='p-8 text-lg font-bold' disabled={animating} onClick={() => handleButton('forward')}>
+            {'>'}
           </button>
         </div>
       </motion.div>
