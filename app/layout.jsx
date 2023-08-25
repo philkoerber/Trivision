af6a7d99a55dadfd342eff3d5d44499b7969d07c
philkoerber <philipp.koerber.93@gmail.com>
2023-08-26 00:50:59 +0900

    opting in for caching for later cookies

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,10 +15,6 @@ const vollkorn = Vollkorn({
 
 import ClientParent from './ClientParent'
 
-export const dynamic = 'force-dynamic'
-export const fetchCache = 'force-no-store'
-export const revalidate = 0
-
 export default function RootLayout({ children }) {
   return (
     <html lang='en' className='antialiased'>
