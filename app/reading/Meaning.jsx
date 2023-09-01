08e03497863791509da35511b010ea2b086819a4
philkoerber <philipp.koerber.93@gmail.com>
2023-09-01 19:30:46 +0900

    design work

--- app/reading/Meaning.jsx
+++ app/reading/Meaning.jsx
@@ -32,12 +32,12 @@ function Meaning() {
   return (
     <AnimatePresence>
       <div className='absolute bottom-[10%] md:bottom-[5%] flex justify-center items-center flex-col w-full '>
-        <div className='w-full max-w-[600px] overflow-y-scroll overflow-x-hidden max-h-[40vh] md:max-h-[33.3vh] backdrop-blur-md rounded  flex justify-center items-center'>
+        <div className='w-full max-w-[600px] overflow-y-scroll overflow-x-hidden max-h-[40vh] md:max-h-[33.3vh] bg-gray-500 rounded flex justify-center items-center'>
           <motion.div
             className=' relative text-md md:text-lg lg:text-xl xl:text-2xl w-screen max-w-[600px] h-fit flex justify-center p-4 md:p-4 lg:p-6 z-60 bg-clip-text text-transparent'
             style={{
               backgroundImage: `url(./noise.png)`,
-              filter: 'contrast(0.7) brightness(2)',
+              filter: 'contrast(0.3) brightness(2) invert(1)',
             }}
           >
             {readingState > 0 ? (readingState !== 4 ? reading.cards[readingState - 1].meaning : reading.meaning) : null}
@@ -46,7 +46,7 @@ function Meaning() {
 
         <div className='flex z-100'>
           <button
-            className='pt-2 w-full z-100 flex scale-75 flex-1 transition duration-800 '
+            className='pt-2 w-full z-100 flex flex-1 transition duration-800 '
             disabled={animating}
             onClick={() => handleButton('backward')}
           >
