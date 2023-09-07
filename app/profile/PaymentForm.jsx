448015a5afcfd92d67ac87a3ea593f308fb381fd
philkoerber <philipp.koerber.93@gmail.com>
2023-09-07 13:51:44 +0900

    install the right paypal package and setup PaymentForm component

---
+++ app/profile/PaymentForm.jsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import {
+  PayPalScriptProvider,
+  PayPalHostedFieldsProvider,
+  PayPalHostedField,
+  usePayPalHostedFields,
+} from '@paypal/react-paypal-js'
+function PaymentForm(props) {
+  return <div>please pay bitch</div>
+}
+
+export default PaymentForm
--- app/profile/page.js
+++ app/profile/page.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import PaymentForm from './PaymentForm'
 
 function page(props) {
+  const clientId = 'okcool'
+  const clientToken = 'yeahthatsnice'
+
   return (
     <div className='w-[500px] bg-neutral-500 flex flex-col gap-4'>
       <div>Hello!</div>
       <div>Tokens: 0</div>
       <div>Readings: 100</div>
-      <div>payment...</div>
+      <div>
+        <PaymentForm clientId={clientId} clientToken={clientToken} />
+      </div>
     </div>
   )
 }
